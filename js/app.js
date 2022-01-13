9f7f80ba217e478bd19f37a2d0a4f6e0dc541c71
EmranCo <alhaddademran@gmail.com>
2022-01-13 13:47:31 +0300

    first commit

--- js/app.js
+++ js/app.js
@@ -5,3 +5,14 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.top = "-500px";
 }
+
+const toggleThem = () => {
+    var body = document.getElementsByTagName('body')[0];
+    if (body.classList.contains('Dark-Them')) {
+        body.classList.remove('Dark-Them');
+        body.classList.add('Light-Them');
+    } else {
+        body.classList.remove('Light-Them');
+        document.getElementsByTagName('body')[0].classList.add('Dark-Them');
+    }
+}
